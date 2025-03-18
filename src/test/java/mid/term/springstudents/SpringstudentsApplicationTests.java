56d7d8d17c16f238ce990e280df57859b633c92d
bendyashka <tu.dastan.tu@gmail.com>
2025-03-18 21:01:25 +0600

    0 commit

---
+++ src/test/java/mid/term/springstudents/SpringstudentsApplicationTests.java
@@ -0,0 +1,13 @@
+package mid.term.springstudents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringstudentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
